17ea6938f68536e7fcb1b01183673e0a6160b14a
Willem Dumee <willemdumee@gmail.com>
2016-03-03 09:22:38 +0100

    added filters to TwigExtension

--- includes/DrifterTwigExtension.php
+++ includes/DrifterTwigExtension.php
@@ -14,8 +14,11 @@ class DrifterTwigExtension extends \Twig_Extension
             new \Twig_SimpleFilter( 'url', array( $this, 'urlFilter' ) ),
             new \Twig_SimpleFilter( 'url_asset', array( $this, 'url_assetFilter' ) ),
             new \Twig_SimpleFilter( 'url_image', array( $this, 'url_imageFilter' ) ),
+            new \Twig_SimpleFilter( 'url_core', array( $this, 'url_coreFilter' ) ),
             new \Twig_SimpleFilter( 't', array( $this, 'translateFilter' ) ),
             new \Twig_SimpleFilter( 'limit', array( $this, 'limitFilter' ) ),
+            new \Twig_SimpleFilter( 'collection_url', array( $this, 'collection_urlFilter' ) ),
+            new \Twig_SimpleFilter( 'paginate', array( $this, 'paginateFilter' ) )
         );
     }
 
@@ -41,6 +44,7 @@ class DrifterTwigExtension extends \Twig_Extension
     {
         $shopid  = str_pad( SHOP_ID, 6, "0", STR_PAD_LEFT );
         $imageid = str_pad( $image, 9, "0", STR_PAD_LEFT );
+        $name = preg_replace("/&([a-z])[a-z]+;/i", "$1", htmlentities(str_replace(' ', '', strtolower($name))));
 
         $url = 'https://static.webshopapp.com/shops/' . $shopid . '/files/' . $imageid . '/' . $directory . '/' . $name . '.jpg';
         if ('' === $directory) {
@@ -54,13 +58,34 @@ class DrifterTwigExtension extends \Twig_Extension
         return $string;
     }
 
-    public function limitFilter( $array, $limit )
+    public function limitFilter( $input, $limit )
     {
-        return array_slice($array, 0, $limit);
+        if (is_array($input)) {
+            return array_slice( $input, 0, $limit );
+        }
+        return substr($input, 0 , $limit);
     }
 
     public function getName()
     {
         return 'drifter_twig_extension';
+    }
+
+    public function collection_urlFilter( $url, $filter )
+    {
+        return $url;
+    }
+
+    public function paginateFilter( $url )
+    {
+        return $url;
+    }
+
+    public function url_coreFilter( $image )
+    {
+
+        $url = 'https://static.webshopapp.com/assets/' . $image;
+
+        return $url;
     }
 }
