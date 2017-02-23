3c5a7aaa05f994730e661915d86cefb4895f599d
Luke Watts <luke@thisis.la>
2017-02-23 08:12:25 +0000

    🔧 Moves shared config out to sketch.config.json

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,51 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect-php');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var flatten = require('gulp-flatten');
 var sourcemaps = require('gulp-sourcemaps');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var csslint = require('gulp-csslint');
-var jshint = require("gulp-jshint");
+var jshint = require('gulp-jshint');
+var url = require('url');
 
-gulp.task('serve', function () {
-    browserSync.init({
-        proxy: "localhost:80/sketch",
-        port: 3001
-    });
+var Config = require('./sketch.config.json');
 
-    gulp.watch("build/scss/**/*.scss", ['sass']);
-    gulp.watch("build/js/**/*.js", ['scripts']);
-    gulp.watch("templates/**/*.rain").on('change', browserSync.reload);
+gulp.task('php', function() {
+    var localUrl = url.parse(Config.localUrl);
+
+    connect.server({
+        hostname: localUrl.hostname,
+        port: localUrl.port
+    }, () => {
+        browserSync.init({
+            proxy: Config.localUrl,
+            port: 3001
+        });
+   });
+})
+
+gulp.task('watch', function () {
+    gulp.watch('build/scss/**/*.scss', ['sass']);
+    gulp.watch('build/js/**/*.js', ['scripts']);
+    gulp.watch('templates/**/*.rain').on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-    return gulp.src("build/scss/**/*.scss")
+    return gulp.src('build/scss/**/*.scss')
         .pipe(flatten())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("assets"))
+        .pipe(gulp.dest('assets'))
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(sourcemaps.write())
         .pipe(rename({
-            suffix: "-min"
+            suffix: '-min'
         }))
-        .pipe(gulp.dest("assets"))
+        .pipe(gulp.dest('assets'))
         .pipe(browserSync.stream());
 });
 
@@ -51,14 +64,14 @@ gulp.task('scripts', function () {
     gulp.src('build/js/**/*.js')
         .pipe(flatten())
         .pipe(minify())
-        .pipe(gulp.dest("assets"))
+        .pipe(gulp.dest('assets'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("jshint", function () {
-    gulp.src("./assets/*.js")
+gulp.task('jshint', function () {
+    gulp.src('./assets/*.js')
         .pipe(jshint())
-        .pipe(jshint.reporter("default"));
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['php', 'watch']);
